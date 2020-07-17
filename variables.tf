57b864c5a0967eb4019709b35563e95cf34c6a73
Sean Sundberg <seansund@us.ibm.com>
2020-07-17 16:53:36 -0500

    Initial release of module (#1)

--- variables.tf
+++ variables.tf
@@ -1,6 +1,27 @@
-#variable "myvariable" {
-#  type        = string
-#  description = "A description of my variable"
-#  default     = ""
-#}
+variable "resource_group_name" {
+  type        = string
+  description = "Resource group where the cluster has been provisioned."
+}
 
+variable "resource_location" {
+  type        = string
+  description = "Geographic location of the resource (e.g. us-south, us-east)"
+}
+
+variable "tags" {
+  type        = list(string)
+  description = "Tags that should be applied to the service"
+  default     = []
+}
+
+variable "name_prefix" {
+  type        = string
+  description = "The prefix name for the service. If not provided it will default to the resource group name"
+  default     = ""
+}
+
+variable "plan" {
+  type        = string
+  description = "The type of plan the service instance should run under (lite, 7-day, 14-day, or 30-day)"
+  default     = "7-day"
+}
